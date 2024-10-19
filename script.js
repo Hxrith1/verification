da769129cfcf54df6bf91e83fa321c91210ec0a5
Hxrith1 <112450620+Hxrith1@users.noreply.github.com>
2024-10-19 19:41:59 +0100

    s

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = 'https://5e3a-81-155-215-249.ngrok-free.app';
+const BACKEND_URL = 'https://c021-185-211-179-90.ngrok-free.app';
 
 async function sendLogToBackend(message) {
     console.log('Log:', message);
